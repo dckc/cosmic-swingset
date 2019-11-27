c7526996d55ea8edfb1eaed938c2a030ea3d626e
Dan Connolly <dckc@madmode.com>
2019-11-26 19:38:55 -0600

    ag-solo-xs.mk: fix debug / release build modes

--- ag-solo-xs.mk
+++ ag-solo-xs.mk
@@ -3,12 +3,17 @@ export PATH := $(MODDABLE)/build/bin/lin/release:$(PATH)
 
 PKG=cosmic-swingset
 
-MODE=debug
+
 BASEDIR=t3
-do-build:
+release-build:
+	mcconfig -p x-cli-lin ag-solo-xs-manifest.json
+	cd $(MODDABLE)/build/tmp/lin/release/$(PKG) && $(MAKE)
+	$(MODDABLE)/build/bin/lin/release/$(PKG) $(BASEDIR)
+
+debug-build:
 	mcconfig -d -p x-cli-lin ag-solo-xs-manifest.json
-	cd $(MODDABLE)/build/tmp/lin/$(MODE)/$(PKG) && $(MAKE)
-	$(MODDABLE)/build/bin/lin/$(MODE)/$(PKG) $(BASEDIR)
+	cd $(MODDABLE)/build/tmp/lin/debug/$(PKG) && $(MAKE)
+	$(MODDABLE)/build/bin/lin/debug/$(PKG) $(BASEDIR)
 
 clean:
 	-rm -rf $(MODDABLE)/build/tmp/lin/release/$(PKG)
